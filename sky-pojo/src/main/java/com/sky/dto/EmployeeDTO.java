d9e35d1f3da33dcb92a54ee4034676720f472bd2
cookies <siriforever@outlook.com>
2025-07-22 20:14:14 +0800

    update: 新增员工功能开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -2,9 +2,9 @@ package com.sky.dto;
 
 import lombok.Data;
 
-import java.io.Serializable;
+import java.io.Serializable; // 序列化，将复杂对象转化成字节流，方便存储或者网络传输(redis, mysql)
 
-@Data
+@Data // 自动生成该类的常用代码：比如getter()和setter()方法，toString(),equals()等方法
 public class EmployeeDTO implements Serializable {
 
     private Long id;
