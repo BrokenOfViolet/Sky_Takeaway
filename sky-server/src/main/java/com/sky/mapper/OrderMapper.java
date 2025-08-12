21bd45f92e2bc0f4eda92f70d2c3a7087224209a
Cookies <siriforever@outlook.com>
2025-08-12 19:06:54 +0800

    feat: 完善微信支付代码(个人无法申请)

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -31,4 +31,12 @@ public interface OrderMapper {
      */
     void update(Orders orders);
 
+    /**
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
 }
